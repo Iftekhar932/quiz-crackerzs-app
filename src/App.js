f342d4c715307979ee96a34845834b14edc68dae
Iftekhar Bin Aziz <iftekhar932@gmail.com>
2023-01-11 17:20:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-3 gap-4">
+      <div className="bg-gray-500">
+        <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
+          <input
+            id="bordered-radio-1"
+            type="radio"
+            value=""
+            name="bordered-radio"
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor="bordered-radio-1"
+            className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Default radio
+          </label>
+        </div>
+        <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
+          <input
+            id="bordered-radio-2"
+            type="radio"
+            value=""
+            name="bordered-radio"
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor="bordered-radio-2"
+            className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Checked state
+          </label>
+        </div>
+        <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
+          <input
+            id="bordered-radio-1"
+            type="radio"
+            value=""
+            name="bordered-radio"
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor="bordered-radio-1"
+            className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Default radio
+          </label>
+        </div>
+        <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
+          <input
+            id="bordered-radio-2"
+            type="radio"
+            value=""
+            name="bordered-radio"
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor="bordered-radio-2"
+            className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Checked state
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+/* 
+
+1. favicon
+2. nav with different name icon
+3. below nav there should be some text while in home route  
+4. 
+
+*/
