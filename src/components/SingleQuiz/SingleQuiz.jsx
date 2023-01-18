8e3a24712d7e16b26fbfd8c3d23af9d4329087bd
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-01-18 21:19:18 +0600

    index collection reamining

--- src/components/SingleQuiz/SingleQuiz.jsx
+++ src/components/SingleQuiz/SingleQuiz.jsx
@@ -4,6 +4,21 @@ const SingleQuiz = ({ quiz }) => {
   const [isTrue, setIsTrue] = useState(false);
   const { question, correctAnswer, options } = quiz;
 
+  // answer validation and count correct answers
+  let correctAnswerCount = 0;
+  const answerCollection = (e, index) => {
+    console.log(e, index);
+  };
+
+  const validateOnSubmit = (e) => {
+    const answerMatch = e.currentTarget.value == correctAnswer ? true : false;
+
+    correctAnswerCount = answerMatch
+      ? correctAnswerCount + 1
+      : correctAnswerCount;
+    console.log(answerMatch, correctAnswerCount);
+  };
+
   return (
     <div className="w-9/12 mx-auto mb-12">
       <h3 className="mb-4 mt-12  font-semibold text-gray-900 dark:text-white flex flex-row justify-between">
@@ -17,14 +32,21 @@ const SingleQuiz = ({ quiz }) => {
             alt="correct answer"
             height="20"
             width="20"
-            className="dark:bg-gray-300 rounded-xl "
+            className="dark:bg-gray-300 rounded-xl"
           />
         </span>
-        <span className={` ${isTrue ? "block" : "hidden"}`}>
+        <span
+          className={`bg-green-200 shadow-lg shadow-green-200 duration-500 ${
+            isTrue ? "opacity-100" : "opacity-0"
+          }`}
+        >
           {correctAnswer}
         </span>
       </h3>
-      <ul className="shadow-md  items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+      <ul
+        onSubmit={validateOnSubmit}
+        className="shadow-md  items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+      >
         {options.map((option, index) => {
           return (
             <li
@@ -35,9 +57,11 @@ const SingleQuiz = ({ quiz }) => {
                 <input
                   id="horizontal-list-radio-license"
                   type="radio"
-                  value=""
+                  value={option}
                   name="list-radio"
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  onClick={(e) => answerCollection(e, index)}
+                  required
                 />
                 <label
                   htmlFor="horizontal-list-radio-license"
