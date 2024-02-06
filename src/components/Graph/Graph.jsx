ce780519909f70a64791e43fdecc09d5aa30208f
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2024-02-06 11:43:51 +0600

    changed comments and state bad practice solved

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import {
   CartesianGrid,
   Legend,
@@ -14,14 +14,16 @@ const Graph = ({ quizData }) => {
   const data = quizData.data;
 
   return (
-    <div className="mx-auto mt-12 w-40">
-      <LineChart width={400} height={400} data={data}>
-        <Line type="monotone" dataKey="total" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" />
-        <XAxis datakey="name" />
-        <YAxis />
-        <Tooltip cursor={{ stroke: "red", strokeWidth: 2 }} label="total" />
-      </LineChart>
+    <div className="mx-auto mt-12 w-1/3">
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={data}>
+          <Line type="monotone" dataKey="total" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip cursor={{ stroke: "red", strokeWidth: 2 }} />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
--- src/components/SingleCategoryCard/SingleCategoryCard.jsx
+++ src/components/SingleCategoryCard/SingleCategoryCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+/* individual quiz category box  */
 const SingleCategoryCard = ({ quiz }) => {
   const { name, id, logo, total } = quiz;
 
@@ -23,7 +24,7 @@ const SingleCategoryCard = ({ quiz }) => {
           to={`/quizzes/${id}`}
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Read more
+          Try some Quizzes
           <svg
             aria-hidden="true"
             className="w-4 h-4 ml-2 -mr-1"
